34a2e841c9f1cd3b0dee8403379f50c17efec734
Martin Basterrechea <mbs@reshapebiotech.com>
2023-02-15 20:40:07 +0100

    add additional match types

--- src/parse/parser.rs
+++ src/parse/parser.rs
@@ -12,6 +12,8 @@ fn decimal_err_into_str(err: Error) -> &'static str {
             "precision necessary to represent exceeds the maximum possible"
         }
         Error::ErrorString(_) => "cannot parse as decimal (unable to display root cause)",
+        Error::Underflow => "cannot parse as decimal (unable to display root cause)",
+        Error::ConversionTo(_) => "cannot parse as decimal (unable to display root cause)",
     }
 }
 
