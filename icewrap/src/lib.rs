4bfeca6202af42f3acacce965ee82b2f005449e8
Sameer Puri <sameer@users.noreply.github.com>
2024-05-20 23:48:39 -0700

    icewrap: Write crate docs

--- icewrap/src/lib.rs
+++ icewrap/src/lib.rs
@@ -1,4 +1,43 @@
 //! A Rust port of the [Heatshrink](https://github.com/atomicobject/heatshrink/) compression library for embedded/real-time systems.
+//!
+//! # How it works
+//!
+//! Heatshrink is based on [LZSS](https://en.wikipedia.org/wiki/Lempel%E2%80%93Ziv%E2%80%93Storer%E2%80%93Szymanski).
+//! It recognizes repeated byte sequences in data and encodes them as _backreferences_ to the original sequence.
+//! A _backreference_ consists of an index and length. The index is a negative offset from the current position in the data that can go as far back as `2^window`.
+//! The length can be any value in the inclusive range `[1, 2^lookahead]`.
+//!
+//! While a backreference of length 1 or 2 could technically be constructed, it would waste space (`sizeof(index) + sizeof(length) > sizeof(single byte)`).
+//! Instead, Heatshrink encodes these smaller sequences of data as _literals_; the raw data is emitted.
+//! To indicate whether data is a backreference or literal, the encoder includes a 1-bit identifer flag: `0` for a backreference, `1` for a literal.
+//!
+//! ## Encoder
+//!
+//! The encoder buffers up incoming data internally to try and generate backreferences in the available window of data.
+//! The search for backreferences is the most computationally intensive operation.
+//! There is an indexing optimization that speeds it up, but it uses more memory.
+//!
+//! ## Decoder
+//!
+//! The decoder immediately yields literals as they are received and maintains a circular buffer of the last `2^window` bytes for handling backreferences.
+//! There aren't really any computationally intensive operations performed.
+//!
+//! ## Raw Format
+//!
+//! ```text
+//! encoded_data = [entry]*
+//! entry = [tag_bit][tag_data]
+//! if tag_bit == [0] then tag_data = [index][len] (backreference)
+//! if tag_bit == [1] then tag_data = [byte] (literal)
+//! index = window-bits number
+//! len = lookahead-bits number
+//! byte = 8-bit number
+//! ```
+//!
+//! # Credits
+//!
+//! Scott Vokes is the author of the original C library.
+//! His [blog post](https://spin.atomicobject.com/heatshrink-embedded-data-compression/) goes into more detail on the origins and performance.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
@@ -26,15 +65,15 @@ mod consts {
     pub(crate) const U8_BITS: u8 = u8::BITS as u8;
 }
 
-/// Internal constants exposed for documentation purposes
+/// Internal constants exposed for documentation purposes.
 pub mod _consts {
     pub use crate::consts::{MAX_WINDOW_BITS, MIN_LOOKAHEAD_BITS, MIN_WINDOW_BITS};
 }
 
-/// Decode data encoded with Heatshrink
+/// Decode data encoded with Heatshrink.
 pub mod decode;
 
-/// Encode data in Heatshrink
+/// Encode data in Heatshrink.
 pub mod encode;
 
 #[cfg(all(feature = "std", test))]
